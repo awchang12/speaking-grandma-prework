3bff9c633661c4ac83ef4cd95737579432cbc2ac
Anthony Chang <awchang12@gmail.com>
2018-04-29 15:22:34 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,8 @@
 # Write a speak_to_grandma method.
 string = "I LOVE YOU GRANDMA!"
-if "I LOVE YOU GRANDMA!"
+if string
+  puts "I LOVE YOU TOO PUMPKIN!"
+end
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
